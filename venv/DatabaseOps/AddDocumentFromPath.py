4361a81f5a89214b6f9469e98608c50b5a34e253
Jack Newton <jack.newton@thameswater.co.uk>
2026-02-27 10:46:39 +0000

    Document loader mod

--- venv/DatabaseOps/AddDocumentFromPath.py
+++ venv/DatabaseOps/AddDocumentFromPath.py
@@ -4,7 +4,11 @@ import chromadb
 client = chromadb.PersistentClient(path="~/AI_Resources/ChromaDB/PersistentClient/Data")
 collection = client.create_collection(name="docs")
 
+document = open("~/AI_Resources/ChromaDB/PersistentClient/Documents/demotextfile.txt")
+
 collection.update(
     ids=["100"],
-    documents=[open(path="~/AI_Resources/ChromaDB/PersistentClient/Documents/demotextfile.txt").read()]
+    documents=[document.read()]
 )
+
+document.close()
